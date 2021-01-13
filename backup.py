ba160d5afef2aa5a563bb3ec0de5d2978f41363c
Martie <martijn9873@hotmail.com>
2021-01-13 22:03:17 +0100

    Update backup.py

--- backup.py
+++ backup.py
@@ -10,10 +10,14 @@ drive = GoogleDrive(gauth)
 folder_id = '123123123123'
 file_metadata = {
     'name': 'backup.tar.gz',
-    'parents': [folder_id]
+    'parents':  [{"kind": "drive#fileLink", "id": folder_id}]
 }
 # Create GoogleDriveFile instance with title 'Hello.txt'.
 file1 = drive.CreateFile(file_metadata)
 file1.Upload() # Upload the file.
+permission = file1.InsertPermission({
+                        'type': 'anyone',
+                        'value': 'anyone',
+                        'role': 'reader'})
 print('title: %s, id: %s' % (file1['title'], file1['id']))
 # title: Hello.txt, id: {{FILE_ID}}
