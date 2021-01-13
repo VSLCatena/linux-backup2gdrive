80ad9f914f6ce2250c7c4de0aaa21ecc1d608129
Martie <martijn9873@hotmail.com>
2021-01-13 21:34:25 +0100

    Update backup.py

--- backup.py
+++ backup.py
@@ -7,8 +7,13 @@ scope = ['https://www.googleapis.com/auth/drive']
 gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name("client_secrets.json", scope)
 drive = GoogleDrive(gauth)
 
+folder_id = '123123123123'
+file_metadata = {
+    'name': 'backup.tar.gz',
+    'parents': [folder_id]
+}
 # Create GoogleDriveFile instance with title 'Hello.txt'.
-file1 = drive.CreateFile({'title': 'backup.tar.gz'})
+file1 = drive.CreateFile(file_metadata)
 file1.Upload() # Upload the file.
 print('title: %s, id: %s' % (file1['title'], file1['id']))
 # title: Hello.txt, id: {{FILE_ID}}
