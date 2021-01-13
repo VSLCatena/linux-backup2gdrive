22463d9642adeee48d85431bb7e6a99bd6da11df
Martie <martijn9873@hotmail.com>
2021-01-13 20:50:05 +0100

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,62 +1,10 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from pydrive.drive import GoogleDrive
 
-# [START drive_quickstart]
-from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+# Create GoogleDrive instance with authenticated GoogleAuth instance.
+drive = GoogleDrive(gauth)
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-
-def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('drive', 'v3', credentials=creds)
-
-    # Call the Drive v3 API
-
-    #content#
-    file_metadata = {'name': 'photo.jpg'}
-    media = MediaFileUpload('files/photo.jpg', mimetype='image/jpeg')
-    file = drive_service.files().create(body=file_metadata,media_body=media,fields='id').execute()
-    print 'File ID: %s' % file.get('id')
-
-
-if __name__ == '__main__':
-    main()
-# [END drive_quickstart]
+# Create GoogleDriveFile instance with title 'Hello.txt'.
+file1 = drive.CreateFile({'title': 'Hello.txt'})
+file1.Upload() # Upload the file.
+print('title: %s, id: %s' % (file1['title'], file1['id']))
+# title: Hello.txt, id: {{FILE_ID}}
