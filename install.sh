de5ba2466525a42066d5ec86acf9c95e9ff4111a
Martie <martijn9873@hotmail.com>
2021-01-13 20:14:23 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#https://developers.google.com/drive/api/v3/quickstart/python
+
+sudo apt update && apt upgrade -y
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
