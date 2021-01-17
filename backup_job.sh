a17c047080e5275ee1bafda27b5a931d7f544710
Kipjr <martijn9873@hotmail.com>
2021-01-17 20:23:03 +0000

    Add: logging and current SCRIPTPATH

--- backup_job.sh
+++ backup_job.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if test -f $PWD/backup_job.conf ;
-   then .  $PWD/backup_job.conf
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo -e "Starting backup\n" > $SCRIPTPATH/backup.log
 
-   mv $BACKUP_DIR/backup.tar.gz $BACKUP_DIR/backup.tar.gz.old
-   tar -zcvpf $BACKUP_DIR/backup.tar.gz $TARGET_DIR 
-   /usr/bin/python3.8 $PWD/linux-backup2gdrive/backup.py $folderid $BACKUP_DIR/backup.tar.gz
+if test -f $SCRIPTPATH/backup_job.conf ;
+   then .  $SCRIPTPATH/backup_job.conf
+
+   tar -zcvpf $BACKUP_DIR/backup.tar.gz $TARGET_DIR >> $SCRIPTPATH/backup.log 2>&1
+   sleep 15
+   /usr/bin/python3.8 $SCRIPTPATH/backup.py $folderid $BACKUP_DIR/backup.tar.gz >>$SCRIPTPATH/backup.log 2>&1
+   sleep 60
+   mv $BACKUP_DIR/backup.tar.gz $BACKUP_DIR/backup.tar.gz.old >> $SCRIPTPATH/backup.log 2>&1
 
 fi
